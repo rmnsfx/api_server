e4ee213a3c48dc85555923e2fdfad8f2307fb8f8
rmn <rmswms@yandex.ru>
2024-07-13 19:07:12 +0300

    launch.json

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,23 +7,32 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+// type Config struct {
+// 	Database struct {
+// 		Host        string `yaml:"host" env:"DB_HOST" env-description:"Database host"`
+// 		Port        string `yaml:"port" env:"DB_PORT" env-description:"Database port"`
+// 		Username    string `yaml:"username" env:"DB_USER" env-description:"Database user name"`
+// 		Password    string `env:"DB_PASSWORD" env-description:"Database user password"`
+// 		Name        string `yaml:"db-name" env:"DB_NAME" env-description:"Database name"`
+// 		Connections int    `yaml:"connections" env:"DB_CONNECTIONS" env-description:"Total number of database connections"`
+// 	} `yaml:"database"`
+// 	Server struct {
+// 		Host        string        `yaml:"host" env:"SRV_HOST,HOST" env-description:"Server host" env-default:"localhost"`
+// 		Port        string        `yaml:"port" env:"SRV_PORT,PORT" env-description:"Server port" env-default:"8080"`
+// 	}
+// 	Greeting string `env:"GREETING" env-description:"Greeting phrase" env-default:"Hello!"`
+// }
+
 type Config struct {
-	Database struct {
-		Host        string `yaml:"host" env:"DB_HOST" env-description:"Database host"`
-		Port        string `yaml:"port" env:"DB_PORT" env-description:"Database port"`
-		Username    string `yaml:"username" env:"DB_USER" env-description:"Database user name"`
-		Password    string `env:"DB_PASSWORD" env-description:"Database user password"`
-		Name        string `yaml:"db-name" env:"DB_NAME" env-description:"Database name"`
-		Connections int    `yaml:"connections" env:"DB_CONNECTIONS" env-description:"Total number of database connections"`
-	} `yaml:"database"`
-	Server struct {
-		Host        string        `yaml:"host" env:"SRV_HOST,HOST" env-description:"Server host" env-default:"localhost"`
-		Port        string        `yaml:"port" env:"SRV_PORT,PORT" env-description:"Server port" env-default:"8080"`
-		Address     string        `yaml:"address" env-default:"localhost:8080"`
-		Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-		IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
-	} `yaml:"server"`
-	Greeting string `env:"GREETING" env-description:"Greeting phrase" env-default:"Hello!"`
+    Env         string `yaml:"env" env-default:"development"`
+    StoragePath string `yaml:"storage_path" env-required:"true"`
+    HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+    Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+    Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+    IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 
@@ -33,7 +42,6 @@ func LoadConfig() *Config {
 		log.Fatal("CONFIG_PATH is not set")
 	}
 
-	// check if file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", configPath)
 	}
