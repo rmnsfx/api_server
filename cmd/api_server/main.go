21612b2aa075f5eee06325d711f310a1265bf90e
rmn <rmswms@yandex.ru>
2024-07-13 19:42:24 +0300

    slog

--- cmd/api_server/main.go
+++ cmd/api_server/main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"api_server/internal/config"
 	"fmt"
+	"log/slog"
+	"os"
 )
+
+const (
+    envLocal = "local"
+    envDev   = "dev"
+    envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+    case envDev:
+        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+    case envProd:
+        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+    }
+
+    return log
+}
 
 func main() {
 
-	// cleanenv
+	/* cleanenv */
 	cfg := config.LoadConfig()
-	fmt.Println(cfg)
+	// fmt.Println(cfg)
 
-	// slog
+	/* slog */
+	log := setupLogger(cfg.Env)
+    log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+    log.Info("Initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+    log.Debug("Logger debug mode enabled")
 
 	// storage
 
